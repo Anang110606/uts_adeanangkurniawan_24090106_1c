26f81e407dcc54b8afff7f1a4b342448bb11398f
Anang110606 <adeanangkurniawan@gmail.com>
2024-11-06 15:49:26 +0700

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,30 @@
+# for i in range(0,5):
+#     print(f'agus sedih')
+
+#perulangan mengecek bilangan genap\ganjil
+# for a in range(0,15):
+#      if a % 2==0:
+#          print(f'{a} - bilangan genap')
+#      else:
+#          print(f'{a} - bilangan ganjil')
+
+# fruits = ['gandul','nanas','jeruk','markisa']
+
+# for a,buah in enumerate(fruits):
+#     print(f'buah ke-{a} :{buah}')
+
+# biodata = {
+#     'nama': 'anang',
+#     'nim' : '24090106',  
+#     'kelas' : '1c',
+#     'usia' : 18
+# }
+# for label,velue in biodata.items():
+#     print (f'{label} :{velue}')
+
+
+
+
+
+
+
